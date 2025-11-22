511bb0dbbcaecac64be891cbc06372f9e416f16a
Amirhossein Hajlou <hajlouamirhossein@gmail.com>
2025-11-22 13:35:27 +0330

    Uncommect a test

--- tests/calculator_test.cpp
+++ tests/calculator_test.cpp
@@ -23,10 +23,10 @@ TEST_F(CalculatorTest, MultiplicationWorks) {
     EXPECT_EQ(calc.multiply(-2, 3), -6);
 }
 
-// TEST_F(CalculatorTest, DivisionWorks) {
-//     EXPECT_DOUBLE_EQ(calc.divide(10, 2), 5.0);
-//     EXPECT_DOUBLE_EQ(calc.divide(5, 2), 2.5);
-// }
+TEST_F(CalculatorTest, DivisionWorks) {
+    EXPECT_DOUBLE_EQ(calc.divide(10, 2), 5.0);
+    EXPECT_DOUBLE_EQ(calc.divide(5, 2), 2.5);
+}
 
 TEST_F(CalculatorTest, DivisionByZeroThrows) {
     EXPECT_THROW(calc.divide(5, 0), std::invalid_argument);
